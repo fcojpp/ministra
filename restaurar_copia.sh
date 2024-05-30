fae91fee7953a61c6dbd16be2039a2f1e30b08d6
Fcojpp <168424507+fcojpp@users.noreply.github.com>
2024-05-30 19:38:19 +0200

    Update restaurar_copia.sh

--- restaurar_copia.sh
+++ restaurar_copia.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
+# Script para restaurar la copia sql de Minsitra Server
 
-docker exec -it mysql-server ./opt/backup/restaura_sql.sh
+# Ruta de la carpeta que contiene los archivos sql dentro del contenedor
+CARPETA_SQL="/opt/backup"
+
+# Listar archivos sql
+echo "Archivos SQL disponibles:"
+ls "$CARPETA_SQL"/*.sql
+echo
+# Solicitar un nombre de fichero sql a restaurar
+read -p "Ingresa el nombre del archivo SQL que deseas restaurar(solo el nombre del archivo):" ARCHIVO_SQL
+echo
+# Confirmar la restauración
+read -p "Estas seguro que quieres restaurar:(s/n)" opcion
+echo
+case $opcion in
+    s)
+        # Verificar si el archivo existe
+        if [ -f "$CARPETA_SQL/$ARCHIVO_SQL" ]; then
+        # Restaurar la base de datos desde el archivo SQL seleccionado
+        mysql -u root -p'Xj3kBT4nhJ' stalker_db < "$CARPETA_SQL/$ARCHIVO_SQL"
+        echo "Restauración completada!!!"
+        else
+        echo "El archivo $ARCHIVO_SQL no existe en la carpeta."
+        fi
+        ;;
+    n)
+        echo "Saliendo ..."
+        exit 0
+        ;;
+    *)
+        echo "Opción inválida. Intentalo de nuevo."
+        ;;
+esac
