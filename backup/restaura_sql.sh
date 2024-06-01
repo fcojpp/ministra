7fa2a63e4f578192e7129458c88166ff57e99baf
Fcojpp <168424507+fcojpp@users.noreply.github.com>
2024-06-01 17:42:01 +0200

    Update restaura_sql.sh

--- backup/restaura_sql.sh
+++ backup/restaura_sql.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
-# Script para restaurar un archivo SQL en MySQL
-
-# Ruta de la carpeta que contiene los archivos SQL
-CARPETA_SQL="/opt/backup"
-
-# Listar archivos SQL disponibles
+# Script para restaurar la copia sql de Minsitra Server
+clear
+echo
+echo "####################################################"
+echo "Restarurar Copia De La Base De Datos Ministra Server"
+echo "####################################################"
+echo
+# Listar archivos sql
 echo "Archivos SQL disponibles:"
-ls "$CARPETA_SQL"/*.sql
-
-# Solicitar al usuario que seleccione un archivo SQL
-echo "Ingresa el nombre del archivo SQL que deseas restaurar(solo el nombre del archivo):"
-read ARCHIVO_SQL
-
-# Verificar si el archivo existe
-if [ -f "$CARPETA_SQL/$ARCHIVO_SQL" ]; then
-    # Restaurar la base de datos desde el archivo SQL seleccionado
-    mysql -u root -p stalker_db < "$CARPETA_SQL/$ARCHIVO_SQL"
-    echo "Restauración completada."
-else
-    echo "El archivo $ARCHIVO_SQL no existe en la carpeta."
-fi
-
+cd /opt/backup
+ls -1 *.sql
+echo
+# Solicitar un nombre de fichero sql
+read -p "Ingresa el nombre del archivo SQL que deseas restaurar:" archivo_sql
+echo
+read -p "Estas seguro que quieres restaurar:(s/n)" opcion
+echo
+case $opcion in
+    s)
+        # Verificar si el archivo existe
+        if [ -f "/opt/backupL/$archivo_sql" ]; then
+        # Restaurar la base de datos desde el archivo sql seleccionado
+        mysql -u root -p'Xj3kBT4nhJ' stalker_db < "/opt/backupL/$archivo_sql" 2> /dev/null
+        echo "Restauración completada!!!"
+        echo
+        else
+        echo "El archivo $archivo_sql no existe en la carpeta."
+        fi
+        ;;
+    n)
+        echo "Saliendo ..."
+        exit 0
+        ;;
+    *)
+        echo "Opción inválida. Intentalo de nuevo."
+        ;;
+esac
