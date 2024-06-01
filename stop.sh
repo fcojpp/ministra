7c134a2be81530641222394c6bd98461cca4206f
Fcojpp <168424507+fcojpp@users.noreply.github.com>
2024-06-01 17:32:22 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
- #!/bin/bash
-
+#!/bin/bash
+# Detener contenedor ministra-server
 docker stop ministra-server
-
+# Detener contenedor mysql-server
 docker stop mysql-server
-
+# Detener contenedor ntp
+docker stop ntp
 
