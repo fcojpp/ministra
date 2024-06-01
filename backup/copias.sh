9792a034e672572299a905bfc0c6e2f295ef5b2f
Fcojpp <168424507+fcojpp@users.noreply.github.com>
2024-06-01 17:36:11 +0200

    Update copias.sh

--- backup/copias.sh
+++ backup/copias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# Script que se ejecuta dentro del contenedor mysql-server, guarda una copia sql en /opt/backup con el nombre backup_fecha_ejecución.sql 
 /usr/bin/mysqldump --routines -u root -p'Xj3kBT4nhJ' stalker_db > /opt/backup/backup_$(date '+%Y-%m-%d').sql
